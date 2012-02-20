47303dd409ea455e47b5e25dec0110fe37464cb5
baruas <baruas@sbcglobal.net>
2012-02-19 17:09:37 -0800

    Initial commit for 3.2.1

--- README.markdown
+++
@@ -1,5 +0,0 @@
-# Ruby on Rails Tutorial: first application
-
-This is the first application for
-[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
-by [Santanu Barua](http://ripple.com) on (11/15/2011).
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'c5ea5ed390caf0d80f259d17a4115cded269e04b13d605ba54bd3d8ea0d1b2a801058e88c14c255bc4173422c017b722f1bcd9c1b7946d2c42a7895aa10e8487'
+FirstApp::Application.config.secret_token = '87833dbb38cc3753e22c01af1ec7d9050489e3ea9724045cabc06316c67f17031e3edf6644696b4579e4cfd3403fc96372c998268dd8498b7421d0b3dda349d1'
